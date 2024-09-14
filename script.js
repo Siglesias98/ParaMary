2a86bbc0d6ba61e5e11220c4fdb522b000e06395
Siglesias98 <155172953+Siglesias98@users.noreply.github.com>
2024-09-14 18:57:20 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function deslizarPagina() {
-    // Desliza suavemente hacia la sección de la flor
-    document.getElementById("imagen-flor").scrollIntoView({ behavior: "smooth" });
-}
+document.addEventListener("DOMContentLoaded", function() {
+    // Función para deslizar suavemente hacia la sección de la flor
+    function deslizarPagina() {
+        document.getElementById("imagen-flor").scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Agregar el event listener al botón para ejecutar la función cuando se haga clic
+    document.querySelector(".boton").addEventListener("click", deslizarPagina);
+});
